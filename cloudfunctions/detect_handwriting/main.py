f0b296bd86da316d61c99f2539583c95bcb6c032
erik sandberg <eriksandbergum@gmail.com>
2020-05-24 09:43:13 -0500

    Can perform text detection on image. Also updated the path where the image is stored if in the Cloud, as Cloud Functions has only one writable directory: /temp

--- cloudfunctions/detect_handwriting/main.py
+++ cloudfunctions/detect_handwriting/main.py
@@ -1,5 +1,10 @@
 def get_pdf_image(pdf_image_url, local=False, cred_file=''):
     from google.cloud import storage
+    if local:
+        output_filename = './temp.png'
+    else:
+        # Cloud Function environment only allows write to /tmp folder
+        output_filename = '/tmp/temp.png'
     # If you don't specify credentials when constructing the client, the
     # client library will look for credentials in the environment.
     if local:
@@ -12,9 +17,20 @@ def get_pdf_image(pdf_image_url, local=False, cred_file=''):
     print(buckets)
     bucket = storage_client.get_bucket('oneline-server-test')
     blob = bucket.blob(pdf_image_url)
-    blob.download_to_filename('./temp.png')
+    blob.download_to_filename(output_filename)
+    return output_filename
 
-    print('ay', bucket)
+def perform_cloud_vision(image_filename, local=False, cred_file=''):
+    from google.cloud import vision
+    from google.cloud.vision import types
+    client = vision.ImageAnnotatorClient.from_service_account_file(cred_file)
+    with open(image_filename, 'rb') as f:
+        content = f.read()
+    image = vision.types.Image(content=content)
+    response = client.document_text_detection(image=image)
+    doc_text = response.full_text_annotation.text
+    print(doc_text)
+    return doc_text
 
 
 def detect_handwriting(request):
--- cloudfunctions/detect_handwriting/test.py
+++ cloudfunctions/detect_handwriting/test.py
@@ -1,7 +1,8 @@
-from main import get_pdf_image
+from main import get_pdf_image, perform_cloud_vision
 
 def test_local():
-    get_pdf_image('test.png', True, '/Users/eriksandberg/Downloads/online-server-test-5be6288e792b.json')
+    local_image = get_pdf_image('test.png', True, '/Users/eriksandberg/Downloads/online-server-test-5be6288e792b.json')
+    output_text = perform_cloud_vision(local_image, True, '/Users/eriksandberg/Downloads/online-server-test-5be6288e792b.json')
 
 if __name__ == "__main__":
     test_local()
